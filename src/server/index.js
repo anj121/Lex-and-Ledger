d1f4049e0d720e70a417731080f6b1389a1c1cbf
anjali <anjalishree64@gmail.com>
2026-02-22 01:07:29 +0530

    fix .env

--- src/server/index.js
+++ src/server/index.js
@@ -14,8 +14,8 @@ import bundleRoutes from "./routes/bundleRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, "../../.env") });
-
+// dotenv.config({ path: path.join(__dirname, "../../.env") });
+dotenv.config();
 const app = express();
 
 /* ------------------ MIDDLEWARES ------------------ */
